9fce5ff118cb413cb822ca0473e5f9e64a41f197
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-12-31 18:40:46 +0100

    Add debug commands for food poisoning & well fed

--- Patches/DebugCommandPatches.cs
+++ Patches/DebugCommandPatches.cs
@@ -7,10 +7,13 @@ namespace HungerRevamped {
 		[HarmonyPatch(typeof(ConsoleManager), "RegisterCommands")]
 		private static class AddConsoleCommands {
 			private static void Postfix() {
-				uConsole.RegisterCommandReturn("get_stored_calories", new uConsole.DebugCommandReturn(GetStoredCalories));
-				uConsole.RegisterCommand("set_stored_calories", new uConsole.DebugCommand(SetStoredCalories));
-				uConsole.RegisterCommandReturn("get_calorie_burn", new uConsole.DebugCommandReturn(GetCalorieBurnRate));
-				uConsole.RegisterCommandReturn("get_calorie_transfer", new uConsole.DebugCommandReturn(GetCalorieTransferRate));
+				uConsole.RegisterCommandReturn("get_stored_calories", GetStoredCalories);
+				uConsole.RegisterCommand("set_stored_calories", SetStoredCalories);
+				uConsole.RegisterCommandReturn("get_calorie_burn", GetCalorieBurnRate);
+				uConsole.RegisterCommandReturn("get_calorie_transfer", GetCalorieTransferRate);
+				uConsole.RegisterCommandReturn("get_well_fed_hunger_score", GetWellFedHungerScore);
+				uConsole.RegisterCommand("set_well_fed_hunger_score", SetWellFedHungerScore);
+				uConsole.RegisterCommand("get_deferred_food_poisonings", GetDeferredFoodPoisonings);
 			}
 
 			private static object GetStoredCalories() {
@@ -32,6 +35,27 @@ namespace HungerRevamped {
 
 			private static object GetCalorieTransferRate() {
 				return HungerRevamped.Instance.GetStoredCaloriesChangePerHour();
+			}
+
+			private static object GetWellFedHungerScore() {
+				return HungerRevamped.Instance.wellFedHungerScore;
+			}
+
+			private static void SetWellFedHungerScore() {
+				if (uConsole.GetNumParameters() == 0)
+					return;
+
+				float hungerScore = uConsole.GetFloat();
+				hungerScore = Mathf.Clamp(hungerScore, -1, 1);
+				HungerRevamped.Instance.wellFedHungerScore = hungerScore;
+			}
+
+			private static void GetDeferredFoodPoisonings() {
+				float timeNow = GameManager.GetTimeOfDayComponent().GetHoursPlayedNotPaused();
+				foreach (DeferredFoodPoisoning dfp in HungerRevamped.Instance.deferredFoodPoisonings) {
+					float dHours = dfp.start - timeNow;
+					uConsole.Log("Hour " + dfp.start + " caused by " + dfp.cause + " (in " + dHours + " hours)");
+				}
 			}
 		}
 	}
